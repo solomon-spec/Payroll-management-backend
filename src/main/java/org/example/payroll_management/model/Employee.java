73bc51e880cd14f04735c1b57d74d7173ca48f7d
solomon Abate <solomonabate18@gmail.com>
2024-05-09 21:00:14 +0300

    Add Employee entity

---
+++ src/main/java/org/example/payroll_management/model/Employee.java
@@ -0,0 +1,41 @@
+package org.example.payroll_management.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String email;
+
+    private LocalDate dateOfJoining;
+
+    private Double salary;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String position;
+
+    private String bankAccount;
+}
