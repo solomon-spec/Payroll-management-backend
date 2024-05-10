ca98d0725de3bf5c8d134c58c8bee59db0fbb04a
solomon Abate <solomonabate18@gmail.com>
2024-05-10 09:11:14 +0300

    Hibernate configuration

--- src/main/java/org/example/payroll_management/PayrollManagementSystemApplication.java
+++ src/main/java/org/example/payroll_management/PayrollManagementSystemApplication.java
@@ -1,5 +1,9 @@
 package org.example.payroll_management;
 
+import org.example.payroll_management.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +12,11 @@ public class PayrollManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PayrollManagementSystemApplication.class, args);
+        SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Employee.class).buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.getTransaction().commit();
+        session.close();
     }
 
 }
