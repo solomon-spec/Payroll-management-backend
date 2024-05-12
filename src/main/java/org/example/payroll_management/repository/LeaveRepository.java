9713f64c2a88acf6fe8959bdf117f1909a54fa21
solomon Abate <solomonabate18@gmail.com>
2024-05-12 11:41:25 +0300

    Add complex query filtering and remove unnecessary function for leave repository

--- src/main/java/org/example/payroll_management/repository/LeaveRepository.java
+++ src/main/java/org/example/payroll_management/repository/LeaveRepository.java
@@ -1,20 +1,18 @@
 package org.example.payroll_management.repository;
 
 import org.example.payroll_management.model.Leave;
-import org.example.payroll_management.model.LeaveStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface LeaveRepository extends JpaRepository<Leave, Long> {
+public interface LeaveRepository extends JpaRepository<Leave, Long>,   JpaSpecificationExecutor<Leave>{
     List<Leave> findByEmployeeId(Long employeeId);
     @Query("SELECT l FROM Leave l WHERE l.status = 'APPROVED' AND :currentDate BETWEEN l.startDate AND l.endDate")
     List<Leave> findApprovedLeavesInEffect(LocalDate currentDate);
-    @Query("SELECT l FROM Leave l WHERE l.status = 'APPROVED' AND l.employee.id = :employeeId AND :currentDate BETWEEN l.startDate AND l.endDate")
-    List<Leave> findApprovedLeavesInEffectForEmployee(Long employeeId, LocalDate currentDate);
 
 }
