f812230d070a2ec9920e92efb6c1a68a5ef3dba3
solomon Abate <solomonabate18@gmail.com>
2024-05-13 14:33:21 +0300

    employee repository

---
+++ src/main/java/org/example/payroll_management/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package org.example.payroll_management.repository;
+
+import org.example.payroll_management.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+   @Query("SELECT e FROM Employee e JOIN e.leaves l WHERE l.status = 'APPROVED' AND CURRENT_DATE BETWEEN l.startDate AND l.endDate")
+    List<Employee> findEmployeesOnLeave();
+
+    List<Employee> findByManagerId(Long managerId);
+}
